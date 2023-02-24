0d52a3b5a7ffb271e7858d90fd7a39a6537d6e0f
lhjwork <jin.come.up.business@gmail.com>
2023-02-24 14:17:30 +0900

    css -> bootstrap

--- logout.php
+++ logout.php
@@ -6,4 +6,6 @@ $http_host = $_SERVER['HTTP_HOST'];
 $host = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http").'://'.$http_host.'ez_1';
 
 $host = 'http://localhost/ez_jin';
+
+
 ?>
