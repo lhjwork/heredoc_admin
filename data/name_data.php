1a58158f54b47f1d778cc3eb61115de301f99df1
lhjwork <jin.come.up.business@gmail.com>
2023-02-27 18:25:18 +0900

    name_data.php

--- data/name_data.php
+++ data/name_data.php
@@ -35,7 +35,36 @@
 	</div>
 
 
+    <!--  -->
     <script>
-        
+
+        function name_update() {
+
+            if (document.getElementById("content_name").value == "" ){
+                alert("이름을 입력해주세요.");
+                return false;
+
+            }else{
+                var t_no = document.getElementById("content_no").value;
+                var t_name = document.getElementById("content_name").value;
+
+                $.ajax({
+                    url:'<?echo $host?>/function/name_update.php',
+                    dataType:'json',
+                    type:'POST',
+                    data:{'t_no':t_no, 't_name':t_name},
+
+                    success:function(result){
+                        if(result == 200){
+                            alert('수정되었습니다.');
+                            window.location.reload();
+                        }
+                    }
+                });
+            }
+        }
+
+
+
     </script>
 	
