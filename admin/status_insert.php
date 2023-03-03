9e4d227e7d67f0fb4b0b6eb73405236509468cad
lhjwork <jin.come.up.business@gmail.com>
2023-03-03 14:46:47 +0900

    status_insert.php 작업 wnd

---
+++ admin/status_insert.php
@@ -0,0 +1,16 @@
+<? 
+
+include_once('../head.php');
+include_once('../menu.php'); 
+
+ ?>
+
+
+
+
+<script src="<?echo $host;?>/css/bower_components/jquery/dist/jquery.min.js"></script>
+<?
+		
+		include_once('../tail.php');
+		
+?>
