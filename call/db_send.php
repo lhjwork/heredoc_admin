89856fcd583344708f8e355573abc4246180d437
lhjwork <jin.come.up.business@gmail.com>
2023-03-06 11:36:20 +0900

    db_send.php

--- call/db_send.php
+++ call/db_send.php
@@ -13,6 +13,50 @@
 ?>
 
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.js"></script>
+<script type="text/javascript">
+function all_db_update() {
+
+    var t_doc_array = Array();
+    var send_cnt = 0;
+    var chkbox = $(".checkSelect");
+
+    for (i = 0; i < chkbox.length; i++) {
+        if (chkbox[i].checked == true) {
+            t_doc_array[send_cnt] = chkbox[i].value;
+            // 여기서 코드는 왜 이렇게 짜셨는지 조금 이해가 안간다.
+            send_cnt++;
+        }
+
+    }
+
+
+    var all_data = {
+        "t_doc_array": t_doc_array,
+        "t_call": document.getElementById("call_select").value
+    };
+
+    if (t_doc_array == '') {
+        alert('배분할 병원을 선택해주세요.');
+    } else {
+        $.ajax({
+            url: "<? echo $host; ?>/call/function/db_send.php",
+            type: "GET",
+            data: all_data,
+            success: function(data) {
+                if (data == "200") {
+                    alert("배분완료 했습니다.");
+                    window.location.reload();
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert("에러가 발생했습니다. \n" + textStatus + " : " + errorThrown);
+                self.close();
+            }
+
+        })
+    }
+}
+</script>
 
 
 
@@ -55,7 +99,70 @@
 
                     </div>
 
-                    <div class="box-body"></div>
+                    <div class="box-body">
+
+                        <? 
+                    // adm_doc 의 t_name, t_doc, adm_db에서 t_call이 배정 받지 않은 count 가져오기 해당팀의
+                    $select= "select t_name, t_doc, (select count(t_no) from adm_db as A where A.t_call = '' and A.t_doc = B.t_doc) as cnt_1 from adm_doc as B where t_team = '$t_team' ORDER BY t_no DESC";
+
+                    $rs = mysqlI_query($connect, $select);
+
+                    while($row = mysqlI_fetch_array($rs)) {
+                        $t_name = $row['t_name'];
+                        $t_doc = $row['t_doc'];
+                        $new_count = $row['cnt_1'];
+
+                        if($new_count != 0){
+									
+                    ?>
+
+
+                        <input type="checkbox" name="checkbox_1[]" id="check_num" value="<? echo $t_doc; ?>"
+                            class="checkSelect" />
+
+
+                        &nbsp;&nbsp;&nbsp;
+                        <? echo $t_name;?> &nbsp;신규 &nbsp;
+                        <? echo $new_count; ?> &nbsp;개<br><br>
+
+
+
+
+                        <? 
+                        	    }
+								
+							}
+						 ?>
+
+                        <div style="float: left; width: 200px;">
+                            <select class="form-control select2" name="call_select" id="call_select">
+
+
+                                <? 
+                                $doc_select = "select t_id, t_name from adm_member where t_access like '%call%'";
+                                $doc_rs = mysqli_query($connect, $doc_select);
+
+                                while($doc_row = mysqli_fetch_array($doc_rs)){
+                        
+                                ?>
+                                <option value="<? echo $doc_row['t_id']; ?>">
+                                    <? echo $doc_row['t_name']; ?>
+                                </option>
+
+                                <? } ?>
+                            </select>
+
+                        </div>
+                        <div style="float: left; width: 100px;margin-left: 2px">
+
+                            <button onclick="javascript:all_db_update()" class="btn btn-primary"
+                                style="width: 100px; margin-left: 2px">배당하기</button>
+                        </div>
+
+
+
+
+                    </div>
 
                 </div>
             </div>
