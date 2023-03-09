bef32cad1837668348454b537e124084e0b19bec
lhjwork <jin.come.up.business@gmail.com>
2023-03-09 10:02:41 +0900

    업무일지 add into call menu

--- menu/menu_call.php
+++ menu/menu_call.php
@@ -106,6 +106,31 @@ if($t_member_access == 'call_pm'){
 
     </li>
 
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-files-o"></i>
+            <span>업무일지 상태 값</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+            <li>
+                <a href="<?echo $host;?>/admin/check_insert.php?check=1">
+                    <i class="fa fa-edit"></i>
+                    <span>콜분류 리스트</span>
+                </a>
+            </li>
+            <li>
+                <a href="<?echo $host;?>/admin/check_insert.php?check=2">
+                    <i class="fa fa-edit"></i>
+                    <span>콜결과 리스트</span>
+                </a>
+            </li>
+
+        </ul>
+    </li>
+
 
     <li class="treeview">
 
